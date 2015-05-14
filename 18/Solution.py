e5f6ba4c561dcb92d5d41af02e95735584a13f54
Daerdemandt <daerdemandt@gmail.com>
2015-05-14 15:58:44 +0300

    18th problem solution

---
+++ 18/Solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def read_triangle():
+	with open("Input.txt") as tfile:
+		return [[int(n) for n in line.strip().split()] for line in tfile]
+
+triangle = read_triangle()
+
+def max_path(row, index):
+	assert row < len(triangle) and index < len(triangle[row])
+	if row + 1 == len(triangle):
+		return triangle[row][index]
+	return triangle[row][index] + max(max_path(row + 1, index), max_path(row + 1, index + 1))
+
+def main():
+	print(max_path(0,0))
+
+main()
